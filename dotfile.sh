eeec5cbcd531927229817bd96d80f4ec2d182177
patrick <63032462+uncertainty-100@users.noreply.github.com>
2022-10-30 12:50:15 -0400

    added script to automate dotfiles

---
+++ dotfile.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+dot_file_dir=~/.dotfiles
+date=$(date --rfc-3339=seconds -u | sed 's/\s/_/g')
+
+for file in $(find $dot_file_dir/home -type f); do
+    # path of the file without the "/home/user" part at the start
+    file_name=$(echo $file | sed "s/$(echo "$dot_file_dir/home/" | sed 's/\//\\\//g')//")
+
+    if [[ -f $HOME/$file_name ]]; then #file exists
+        if [[ -L $HOME/$file_name ]]; then #file is link
+            if [[ $(readlink $HOME/$file) == $file ]]; then continue; fi
+            unlink $HOME/$file_name
+            ln -s $file $HOME/$file_name
+        else
+            mkdir -p $(dirname $dot_file_dir/backup/$date/$file_name)
+            mv $HOME/$file_name $dot_file_dir/backup/$date/$file_name
+            ln -s $file $HOME/$file_name
+        fi
+    else
+        ln -s $file $HOME/$file_name
+    fi
+done
+
+
+
--- home/.bashrc
+++ home/.bashrc
@@ -18,16 +18,26 @@ PS1="\[\e[1;36m\]\u\[\e[0m\]@\[\e[1;36m\]\h \[\e[1;35m\]\W \[\e[0m\]$ "
 . "$HOME/.cargo/env"
 
 HISTCONTROL=ignoredups
-
+export LC_ALL="C.UTF-8"
 
 
 set -o vi
-alias ls='exa'
 alias make="make -j$(python3 -c "import os; print(os.cpu_count())")"
 alias vi='vim'
 
-which powerline-shell 2> /dev/null
-if [[ $? -eq 0 ]] ; then
+if command -v exa &> /dev/null; then
+    alias ls='exa'
+fi
+
+if command -v bat &> /dev/null; then
+    alias ls='bat -P'
+fi
+
+if command -v rg &> /dev/null; then
+    alias ls='rg'
+fi
+
+if command -v powerline-shell &> /dev/null; then
 
     function _update_ps1() {
         # PS1="$(powerline-shell $? | sed 's//?\\[??\\]/g' | sed 's/✎/?\\[??\\]/g')"
